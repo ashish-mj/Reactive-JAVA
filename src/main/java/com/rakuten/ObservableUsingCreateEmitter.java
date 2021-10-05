50e03cba434d8877af30d5dccecf69a2f3e785c9
Ashish M J <ashm.jagadeesh@gmail.com>
2021-10-05 14:43:07 +0530

    Observable using create emitter

---
+++ src/main/java/com/rakuten/ObservableUsingCreateEmitter.java
@@ -0,0 +1,17 @@
+package com.rakuten;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableUsingCreateEmitter {
+
+	public static void main(String[] args) {
+		Observable<String> source = Observable.create(emitter -> {
+		emitter.onNext("one");
+		emitter.onNext("two");
+		emitter.onNext("three");
+		emitter.onComplete();
+		});
+
+	}
+
+}
